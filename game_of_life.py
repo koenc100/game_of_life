45e6f548bb952783456d52bb5e44b7052dfa280f
koenc100 <koen604@gmail.com>
2021-08-09 22:31:07 +0200

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -5,4 +5,15 @@ Created on Mon Aug  9 21:50:44 2021
 @author: koen6
 """
 
-import numpy as np 
+import pygame, sys
+ 
+def main():
+    a = 4 
+    return a
+
+
+if __name__ == '__main__':
+    main()
+    
+    
+    
