5300385c571e065d680a1b8fb2f7afbf8940883a
koenc100 <koen604@gmail.com>
2021-08-11 20:19:14 +0200

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -5,11 +5,23 @@ Created on Mon Aug  9 21:50:44 2021
 @author: koen6
 """
 
+# Imports
 import pygame, sys
+
+# Constants 
+SCREENSIZE = WIDTH, HEIGHT = 600, 400
+BLACK = (0, 0, 0)
+GREY = (160, 160, 160)
+
  
 def main():
-    a = 4 
-    return a
+    pygame.init()
+    # draw the grid 
+    
+    # While loop 
+        # Refresh every time with function "generation" 
+        
+    return 4
 
 
 if __name__ == '__main__':
