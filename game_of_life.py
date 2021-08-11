081331fd6c9deb1a7f08d41d2a4234aaa783f1e9
koenc100 <koen604@gmail.com>
2021-08-11 21:18:03 +0200

    Update game_of_life.py

--- game_of_life.py
+++ game_of_life.py
@@ -9,22 +9,48 @@ Created on Mon Aug  9 21:50:44 2021
 import pygame, sys
 
 # Constants 
-SCREENSIZE = WIDTH, HEIGHT = 600, 400
+SCREENSIZE = WIDTH, HEIGHT = 800, 600
 SCREEN = pygame.display.set_mode(SCREENSIZE)
- 
 
+WHITE = (255, 255, 255)
+GREY = (100, 100, 100)
+
+FPS = 2
+
+
+pygame.display.set_caption("The Game of Life")
+
+def color_screen():
+    SCREEN.fill(GREY)
+    pygame.display.update()
+    
+def draw_grid():
+    pygame.draw.line(SCREEN, (1, 1, 1), 10, 10)
+    pygame.display.update()
+    
 def main():
     
     pygame.init()
+    clock = pygame.time.Clock()
     
     run = True 
+    
     while run:
         
+        clock.tick(FPS)
+        
+        
         for event in pygame.event.get():
+            
+            # If event is quit end the while loop
             if event.type == pygame.QUIT:
                 run = False 
+            
+            color_screen()
                 
-        pygame.quit()
+            
+                
+    pygame.quit()
 
 
 if __name__ == '__main__':
