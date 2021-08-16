c3d4b2dd6d32a3f4990e0c8921dae09a4c2d67e9
koenc100 <koen604@gmail.com>
2021-08-16 19:28:42 +0200

    update

--- game_of_life.py
+++ game_of_life.py
@@ -7,9 +7,11 @@ Created on Mon Aug  9 21:50:44 2021
 
 # Imports of libraries
 import pygame, sys
+from pygame.locals import *
+import numpy as np
 
 # Constants 
-SCREENSIZE = WIDTH, HEIGHT = 720, 720
+SCREENSIZE = WIDTH, HEIGHT = 820, 720
 
 # The main surface to draw on
 SCREEN = pygame.display.set_mode(SCREENSIZE)
@@ -25,8 +27,12 @@ pygame.display.set_caption("The Game of Life")
 def color_screen():
     SCREEN.fill(GREY)
     
-    
 def draw_grid(divisions):
+    
+    """
+    function creates and draws (divisions x divisions) grid 
+    returns cellSize in pixels and a numpy zero matrix with the same dimentions
+    """
 
     CONTAINER_WIDTH_HEIGHT = 700  # Not to be confused with SCREENSIZE
     cont_x, cont_y = 10, 10  # TOP LEFT OF CONTAINER
@@ -68,39 +74,80 @@ def draw_grid(divisions):
           (cont_x, cont_y + (cellSize*x)),
           (cont_x + CONTAINER_WIDTH_HEIGHT, cont_y + (cellSize*x)), 1)
         
+    # Make numpy grid
+    MAP = np.zeros((divisions, divisions))   
+        
+    return cellSize, MAP
+   
+    
+def fill_square(mx, my, cellSize, MAP):
+    
+    """
+    Function fills squares in grid and updates starting matrix
+    """
+    
+    # get [x, y] location of clicked cell
+    x_loc = int((mx - 10)  // cellSize)
+    y_loc = int((my - 10)  // cellSize)
+    
+    # changge selected square from 0 to 1. (columns x rows)
+    MAP[y_loc, x_loc] = 1
+    
+    # define corner locations of square to be filled (+10 for padding)
+    x_coor = x_loc * cellSize + 10
+    y_coor = y_loc * cellSize + 10
+    
+    # Draw rectangle on screen
+    pygame.draw.rect(SCREEN, BLACK, (x_coor, y_coor, cellSize, cellSize))
+    
+    print(MAP)
+    print(MAP.shape)
+    
+def button():
+    return 4
+
+
 def next_generation():
     return 4
 
-def fill_square():
-    # for mouse press, the square gets filled. 
-    # Also, the position in the matrix gets from 0 to 1. 
-    return 4
-        
+
 # The pre function of the game is handled.
 def pre_game():
     
     # Initialize the module
     pygame.init()
-    clock = pygame.time.Clock()
+    clock = pygame.time.Clock() 
     
     # Static part 
     color_screen()
-    draw_grid(70)
+    cellSize, MAP = draw_grid(50)
     
     run = True
     
+    clicking = False
+
     # While loop for selecting rectangles
     while run:
         
         clock.tick(FPS)
         
+        # Get x and y coordinates of mouse
+        mx, my = pygame.mouse.get_pos()
+        
         # Break out of the while loop when game is quit
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+            
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    clicking = True
+                    fill_square(mx, my, cellSize, MAP)
+                    
+                    
                 
         # If mouse gets pressed, fill the rectangle
-        fill_square()
+       # fill_square()
         
         # If button "bring to life" is pressed, main function is started
         
@@ -110,13 +157,13 @@ def pre_game():
                 
     pygame.quit()
 
-def main():
+def protocol():
     
     clock = pygame.time.Clock()
     
     # Static part 
     color_screen()
-    draw_grid(70)
+    draw_grid(30)
     
     run = True 
     
@@ -130,7 +177,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False 
                 
-        # The game of life protecol is run, the cells live 
+        # The game of life protocol is run, the cells live 
         next_generation()
         
         # When the reset button is pressed, the grid returns to its empty state
---
+++ testfile.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 16 18:37:34 2021
+
+@author: koen6
+"""
+
+import numpy as np
+
+A = np.zeros((10, 10))
+
+A[1, 1] = 1
+
+print(A)
